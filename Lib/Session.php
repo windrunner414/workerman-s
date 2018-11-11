cd71fa46c0e04b793f0fd719af2a59f9223a947a
windrunner414 <windrunner414@outlook.com>
2018-11-11 16:59:20 +0800

    add Session

--- Lib/Session.php
+++ Lib/Session.php
@@ -12,9 +12,9 @@ class Session
 {
     public static $path;
     public static $name;
-    public static $gcProbability;
-    public static $gcDivisor;
-    public static $gcMaxLifeTime;
+    public static $gcProbability = 1;
+    public static $gcDivisor = 1000;
+    public static $gcMaxLifeTime = 1440;
 
     static function init()
     {
@@ -25,6 +25,65 @@ class Session
 
         self::$name = ini_get('session.name');
 
+        if ($gc_probability = ini_get('session.gc_probability')) {
+            self::$gcProbability = $gc_probability;
+        }
+
+        if ($gc_divisor = ini_get('session.gc_divisor')) {
+            self::$gcDivisor = $gc_divisor;
+        }
+
+        if ($gc_max_life_time = ini_get('session.gc_maxlifetime')) {
+            self::$gcMaxLifeTime = $gc_max_life_time;
+        }
+
+        if (!is_dir(self::$path)) mkdir(self::$path, 0777, true);
+    }
+
+    static function get()
+    {
+        if (!isset($_COOKIE[self::$name])) return [];
+
+        $sessionId = $_COOKIE[self::$name];
+        $file = self::$path . '/sess_' . $sessionId;
+
+        if (is_file($file)) {
+            $content = file_get_contents($file);
+            if ($content !== false) {
+                $session = \swoole_serialize::unpack($content);
+                if ($session !== false) return $session;
+            }
+        }
+
+        return [];
+    }
+
+    static function set($session)
+    {
+        if (!is_array($session)) return false;
+        if (!isset($_COOKIE[self::$name])) return false;
+
+        $sessionId = $_COOKIE[self::$name];
+        $content = \swoole_serialize::pack($session);
+        return file_put_contents(self::$path . '/sess_' . $sessionId, $content);
+    }
+
+    static function gc()
+    {
+        if (self::$gcDivisor > 0 && self::$gcProbability > 0 && self::$gcMaxLifeTime > 0) {
+            if (rand(1, self::$gcDivisor) <= self::$gcProbability) self::_gc();
+        }
+    }
+
+    static function _gc()
+    {
+        $now = time();
+
+        foreach (glob(self::$path . '/sess_*') as $file) {
+            if (is_file($file) && $now - filemtime($file) > self::$gcMaxLifeTime) {
+                unlink($file);
+            }
+        }
     }
 }
 
